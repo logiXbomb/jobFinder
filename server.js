02fe2c7557cd3c241caf7c0533b0b834042daf2f
igots <seth.lowie@gmail.com>
2015-04-09 18:23:19 +0000

    MongoLab Connection

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./data/models/Job')
 
 var app = express();
 
@@ -7,8 +9,26 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res){
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    })
+});
+
 app.get('*', function(req, res){
     res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://igots:Rattle17@ds041167.mongolab.com:41167/jobfinder');
+
+
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!')
+    jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
